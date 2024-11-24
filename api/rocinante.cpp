bb18e36e8dd28337da707b7d03f017f9cb847a33
Brad Grantham <grantham@plunk.org>
2024-11-24 14:19:17 -0800

    handle unhandled switch default

--- api/rocinante.cpp
+++ api/rocinante.cpp
@@ -12,6 +12,10 @@ void RoVideoSetMode(bool interlaced, RoRowConfig line_config, void* private_data
                 return NTSC_LINE_SAMPLES_912;
             case RO_VIDEO_ROW_SAMPLES_1368:
                 return NTSC_LINE_SAMPLES_1368;
+            default:
+                printf("Received unexpected value %d for line_config in %s.\n", line_config, __FUNCTION__);
+                printf("Returning NTSC_LINE_SAMPLES_912 and hoping for the best!\n");
+                return NTSC_LINE_SAMPLES_912;
         }
     }(line_config);
 
